2211acc228ee801acb0237611ccd77867901fd9a
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2024-10-15 23:31:53 -0800

    Authentication done with Google, Microsoft, GitHub, and Apple providers using WorkOS

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,16 +1,31 @@
 import Link from "next/link";
+import React from 'react';
+import { getSignInUrl, withAuth, signOut} from "@workos-inc/authkit-nextjs";
 
-import React from 'react'
+export default async function Header() {
+    const { user } = await withAuth();
+    const signInUrl = await getSignInUrl();
+    console.log("user and url: ", user, signInUrl);
 
-const Header = () => {
   return (
     <div>
       <header>
+        {/* {JSON.stringify(user)} */}
         <div className="container flex items-center justify-between mx-auto my-4">
             <Link className="font-bold text-xl " href={'/'}>Job Board</Link>
-            <nav className="flex gap-2 *:px-4 *:py-2 *:rounded-md">
-                <Link href={'/login'} className="bg-gray-200 ">Login</Link>
-                <Link href={'/new-listing'} className="bg-blue-500 text-white">Post Job</Link>
+            <nav className="flex gap-2">
+                {!user && (
+                    <Link href={signInUrl} className="bg-gray-200 px-4 py-2 rounded-md">Login</Link>
+                )}
+                {user && (
+                  <form action={async () => {
+                    'use server';
+                     await signOut();
+                  }}>
+                    <button type="submit" className="px-4 py-2 rounded-md bg-gray-200">Logout</button>
+                  </form>
+                )}
+                <Link href={'/new-listing'} className="px-4 py-2 rounded-md bg-blue-500 text-white">Post Job</Link>
             </nav>
         </div>
       </header>
@@ -18,4 +33,3 @@ const Header = () => {
   )
 }
 
-export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import Hero from "./components/Hero";
-import Jobs from './components/Jobs'
+import Jobs from './components/Jobs';
+// import {
+//   getSignInUrl,
+//   getSignUpUrl,
+//   withAuth,
+//   signOut,
+// } from '@workos-inc/authkit-nextjs';
 
-export default function Home() {
+export default async function Home() {
+
+  // const { user } = await withAuth();
+
+  // // Get the URL to redirect the user to AuthKit to sign in
+  // const signInUrl = await getSignInUrl();
+
+  // // Get the URL to redirect the user to AuthKit to sign up
+  // const signUpUrl = await getSignUpUrl();
+
   return (
     <div>
       <Hero/>
